7c5f3cd1fe3917ed872890fbf51d755c99f1cf01
sunnylost <sunnylost@gmail.com>
2024-07-28 22:12:22 +0800

    feat: use bun

--- src/compiler/__test__/lexer.test.ts
+++ src/compiler/__test__/lexer.test.ts
@@ -1,23 +1,24 @@
+import {test,expect} from 'bun:test'
 import Lexer from '../lexer.ts'
 import { TokenType } from '../tokenType.ts'
 
 test('lexer html', () => {
     const lexer = new Lexer('<html lang="en"><body></body></body></html>')
     const tokens = lexer.scanAll()
-    expect(tokens.length).toBe(1)
+    expect(tokens.length).toBe(2)
     expect(tokens[0].type).toBe(TokenType.HTML)
 })
 
 test('lexer comment', () => {
     const lexer = new Lexer('<html lang="en">{# some comment #}</body></html>')
     const tokens = lexer.scanAll()
-    expect(tokens.length).toBe(3)
+    expect(tokens.length).toBe(4)
     expect(tokens[1].type).toBe(TokenType.COMMENT)
 })
 
 test('lexer tag', () => {
     const lexer = new Lexer('<html lang="en">{{ if 1 }}</body></html>')
     const tokens = lexer.scanAll()
-    expect(tokens.length).toBe(3)
+    expect(tokens.length).toBe(4)
     expect(tokens[1].type).toBe(TokenType.TagIf)
 })
